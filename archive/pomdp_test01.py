fcf5ed5af8e2fef0c34fcbb2f550743368044026
Jeremy R Easton-Marks <j.r.eastonmarks@gmail.com>
2023-04-23 14:59:11 -0500

    Clean up

--- archive/pomdp_test01.py
+++
@@ -1,8 +0,0 @@
-# S - State Space {Hungry, Full}
-# A - Action Space {Feed, Ignore}
-# O - Observation Space {Crying, Quiet}
-# T -> Transition Function 
-# R -> Reward Function
-# O -> Observation Function
-# Y -> Discount Factor
-
